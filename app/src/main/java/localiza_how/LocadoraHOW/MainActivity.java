81ed5d694a7301ad27a41e7b44d9d97e0c60dd97
lucasmaiar <lucas.maiar@outlook.com>
2021-06-02 22:56:33 -0300

    Initial commit

---
+++ app/src/main/java/localiza_how/LocadoraHOW/MainActivity.java
@@ -0,0 +1,14 @@
+package localiza_how.LocadoraHOW;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
